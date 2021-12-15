1df6cc1515956b466bc857a099e85836255cd2a4
rnxa <71078372+rnxa@users.noreply.github.com>
2021-12-15 10:47:08 +0700

    remove proxy

--- loai3.sh
+++ loai3.sh
@@ -10,7 +10,4 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/racing -a ethash -o stratum+http://103.147.126.9:8443/a0111 -w trex\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
-sudo systemctl daemon-reload
-sudo systemctl enable racing.service
 ./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x575aD1F3f6C5493fB4EC2969A1701916927A23df -p x &
